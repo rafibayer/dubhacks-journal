f00055ae3f253ad594abd5d9b72d3d8edc034eeb
Sina Jamshidi <hello@sinajamshidi.com>
2019-10-13 05:39:54 -0700

    fix visualize and storage bug. Make 2 datasets for visualization

--- src/visualize.py
+++ src/visualize.py
@@ -7,28 +7,31 @@ import collections
 class Viz():
     def __init__(self):
         self.db = Storage()
-        self.words_dict = db.get_all_words()
-        self.days = db.data["dayEmotions"]
+        self.words_dict = self.db.get_all_words()
+        self.days = self.db.data["dayEmotions"]
 
     def makeGrid(self):
         arr = []
         sad_counter = 0
-        for i range(0,30):
+        for i in range(0,30):
             emotion = 0
             if self.days[str(i)] == "happiness":
                 emotion = 1
-                sad_counter = 0
+                if sad_counter < 5:
+                    sad_counter = 0
             elif self.days[str(i)] == "neutral":
                 emotion = 0
-                sad_counter = 0
+                if sad_counter < 5:
+                    sad_counter = 0
             elif self.days[str(i)] == "surprise":
                 emotion = 0
-                sad_counter = 0
+                if sad_counter < 5:
+                    sad_counter = 0
             else:
                 emotion = -1
                 sad_counter += 1
 
-            arr.append[emotion]
+            arr.append(emotion)
     
         # data = np.array(arr)
         # data = np.reshape(data, (5,6))
@@ -49,7 +52,7 @@ class Viz():
         # ax.set_yticklabels([])
 
         # plt.savefig('../viz/grid.png')
-        if sad_counter > 10:
+        if sad_counter > 5:
             return "You have been feeling down lately, consider looking into some help"
         else:
             return "Glad you're doing okay! Keep it up!"
@@ -58,8 +61,8 @@ class Viz():
         sorted_x = sorted(self.words_dict.items(), key=lambda kv: int(kv[1]))
         saddest = sorted_x[0][0]
         happiest = sorted_x[len(sorted_x)-1][0]
-        middle1 = sorted_x[len(sorted_x)/2][0]
-        middle2 = sorted_x[len(sorted_x)/2 + 1][0]
+        middle1 = sorted_x[int(len(sorted_x)/2)][0]
+        middle2 = sorted_x[int(len(sorted_x)/2 + 1)][0]
         return [happiest, middle1, middle2, saddest]
 
 
